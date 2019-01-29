8775ec99454f2b2fb31a8b8c5e305e6978499c42
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-29 06:57:03 +0545

    Size of Operator Example

---
+++ Data Types/sizeofoperator.cpp
@@ -0,0 +1,11 @@
+//Example of sizeof() operator
+
+#include<stdio.h> 
+int main() 
+{ 
+    printf("%d\n",sizeof(char)); 
+    printf("%d\n",sizeof(int)); 
+    printf("%d\n",sizeof(float)); 
+    printf("%d", sizeof(double)); 
+    return 0; 
+}  
