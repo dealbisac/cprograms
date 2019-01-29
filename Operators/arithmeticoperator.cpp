544a979c043f672b41c6235462a34311a0de274c
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-29 07:40:54 +0545

    Arithmetic Operator Updated

--- Operators/arithmeticoperator.cpp
+++ Operators/arithmeticoperator.cpp
@@ -4,8 +4,9 @@
 
 int main()
 {
- int a = 12, b = 3;
- int addition, subtraction, multiplication, division, modulus;
+ int a = 12, b = 5;
+ int addition, subtraction, multiplication, modulus;
+ float division;
  
  addition = a + b; //addition of 3 and 12
  subtraction = a - b; //subtract 3 from 12
@@ -16,7 +17,7 @@ int main()
  printf("Addition of two numbers a, b is : %d\n", addition);
  printf("Subtraction of two numbers a, b is : %d\n", subtraction);
  printf("Multiplication of two numbers a, b is : %d\n", multiplication);
- printf("Division of two numbers a, b is : %d\n", division);
+ printf("Division of two numbers a, b is : %f\n", division);
  printf("Modulus of two numbers a, b is : %d\n", modulus);
 
 }
