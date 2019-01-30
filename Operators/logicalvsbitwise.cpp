1e96cf9e04a038fd80d7ebcb91de453ed3d23236
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-30 08:33:33 +0545

    Logical vs Bitwise Updated

--- Operators/logicalvsbitwise.cpp
+++ Operators/logicalvsbitwise.cpp
@@ -14,25 +14,27 @@ int main(){
 	printf("a = %d\n", a); //10 because second condition is not checked  
 	
 	result = (a<b) & (a++<c);
-	printf("(a<b) & (a++<c) is %d\n", result);//false && true = false  
+	printf("(a<b) & (a++<c) is %d\n", result);//false & true = false  
 	printf("a = %d\n", a);//11 because second condition is checked 
 	
 	
 	//Difference between || and |
 	printf("\n\nDifference between || and |\n");
-	result = (a>b) || (a<c);
-	printf("(a>b) || (a<c) is %d\n", result);//true || true = true  
+	result = (a>b) || (a++>c);
+	printf("(a>b) || (a++>c) is %d\n", result);//true || false = true  
+	printf("a = %d\n", a); //11 because second condition is not checked
 	
-	result = (a>b) | (a<c);
-	printf("(a>b) | (a<c) is %d \n", result);//true | true = true  
+	result = (a>b) | (a++>c);
+	printf("(a>b) | (a++>c) is %d \n", result);//true | false = true  
+	printf("a = %d\n", a); //12 because second condition is not checked
 	
 	//|| vs |  
 	printf("\n || vs |  \n \n");
 	result = (a>b) || (a++<c);
 	printf("(a>b) || (a++<c) is %d \n", result);//true || true = true  
-	printf("a = %d\n", a);//10 because second condition is not checked  
-	printf("(a>b) || (a++<c) is %d\n", result);//true | true = true  
-	printf("a=%d\n", a);//11 because second condition is checked  
+	printf("a = %d\n", a);//12 because second condition is not checked  
+	printf("(a>b) | (a++<c) is %d\n", result);//true | true = true  
+	printf("a=%d\n", a);//12 because second condition is checked  
 	
 	
 	return 0;
