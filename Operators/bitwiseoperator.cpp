1fec18079f4c914c56fe99869dcef05e4faf49cf
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-30 07:33:45 +0545

    Bit Wise Operator

---
+++ Operators/bitwiseoperator.cpp
@@ -0,0 +1,21 @@
+// C Program to demonstrate the working of bitwise operators
+
+#include <stdio.h>
+int main()
+{
+    int a = 5, b = 5, c = 10, result;
+
+    result = (a == b) & (c > b);
+    printf("(a == b) & (c > b) equals to %d \n", result);
+
+    result = (a == b) & (c < b);
+    printf("(a == b) & (c < b) equals to %d \n", result);
+
+    result = (a == b) | (c < b);
+    printf("(a == b) | (c < b) equals to %d \n", result);
+
+    result = (a != b) | (c < b);
+    printf("(a != b) | (c < b) equals to %d \n", result);
+
+    return 0;
+}
