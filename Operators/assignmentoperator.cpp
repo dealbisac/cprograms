093b96962d0e8fb0a790567133bf39479e780736
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-30 06:57:01 +0545

    Assignment Operator Added

---
+++ Operators/assignmentoperator.cpp
@@ -0,0 +1,27 @@
+// C Program to demonstrate the working of assignment operators
+
+#include <stdio.h>
+int main()
+{
+    int a = 5, c;
+
+    c = a;
+    printf("c = %d \n", c);
+
+    c += a; // c = c+a
+    printf("c = %d \n", c);
+
+    c -= a; // c = c-a
+    printf("c = %d \n", c);
+
+    c *= a; // c = c*a
+    printf("c = %d \n", c);
+
+    c /= a; // c = c/a
+    printf("c = %d \n", c);
+
+    c %= a; // c = c%a
+    printf("c = %d \n", c);
+
+    return 0;
+}
