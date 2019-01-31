42637e335f1768cfe672ba23d5c73c4cd4ea1e81
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-31 07:50:51 +0545

    Comments file updated

--- Comments/comments.cpp
+++ Comments/comments.cpp
@@ -1,9 +1,9 @@
-//Single Line Comment
+//Single Line Comments 
 
 #include<stdio.h>    
 int main(){    
     /*printing information   
       Multi-Line Comment*/  
     printf("Hello C");    
-return 0;  
+	return 0;  
 }      
