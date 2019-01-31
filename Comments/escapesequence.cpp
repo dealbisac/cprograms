e5a5a18ef7954401e6cbc553c3d6829881719ad9
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-01-31 07:44:04 +0545

    Escape Sequence Added

---
+++ Comments/escapesequence.cpp
@@ -0,0 +1,33 @@
+//Escape Sequence Example
+
+#include<stdio.h>    
+int main(){    
+     int number=50; 
+     /*
+     Escape Sequence	Meaning
+     ----------------------------
+	 \a					Alarm or Beep
+	 \b					Backspace
+	 \f					Form Feed
+	 \n					New Line
+	 \r					Carriage Return
+	 \t					Tab (Horizontal)
+	 \v					Vertical Tab
+	 \\					Backslash
+	 \'					Single Quote
+	 \"					Double Quote
+	 \?					Question Mark
+	 \nnn				octal number
+	 \xhh				hexadecimal number
+	 \0					Null
+     
+     
+     */
+     
+	 printf("\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"); 
+    printf("You\nare\nlearning\n\'c\' language\n\"Do you know C language\"");  
+	
+return 0;  
+}     
+
+
