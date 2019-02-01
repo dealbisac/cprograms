385c1847bca7d2128a42f1ceafc7a02be31db7aa
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-01 07:40:48 +0545

    Constant using const

--- Constant/constantusingconst.cpp
+++ Constant/constantusingconst.cpp
@@ -1,11 +1,11 @@
 // Constant using const keyword
 
 #include<stdio.h>    
-int main(){    
-
-const float PI=3.14;     
-PI=4.5;    
-printf("The value of PI is: %f",PI);    
-    return 0;  
-} 
+int main()
+{    
+	const float PI=3.14;     
+	PI=4.5;    
+	printf("The value of PI is: %f",PI);    
+    
+	return 0;  
 }     
