88cb29fc14577a84ac43510fd96a6717847a792b
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-01 08:14:11 +0545

    Challanges-1 Added

---
+++ Challange-1/questions.cpp
@@ -0,0 +1,16 @@
+// Do the following program in your own way
+
+// C Program to Print a Sentence
+// C Program to Print a Integer Entered by a User
+// C Program to Add Two Integers Entered by User
+// C Program to Multiply two Floating Point Numbers
+// C Program to Find ASCII Value of Character Entered by User
+// C Program to Find Quotient and Remainder of Two Integers Entered by User
+// C Program to Find Size of int, float, double and char of Your System
+// C Program to Demonstrate the Working of Keyword long
+// C Program to Swap Two numbers Entered by User
+// C Program to show the example of ternary operator
+// C Program to show the examples of operators
+// C Program to show the example of escape sequence
+// C Program to show the example of Constant Value.
+ 
