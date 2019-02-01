2637abda58ee6b1116e2dae667911852f6a979a8
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-01 08:06:38 +0545

    Operation using the define

---
+++ Constant/definesecondexample.cpp
@@ -0,0 +1,9 @@
+//Value defined using the operations
+
+#include <stdio.h>  
+#define MIN(a,b) ((a)<(b)?(a):(b))  
+int main() 
+{  
+   printf("Minimum between 10 and 20 is: %d\n", MIN(10,20)); 
+   return 0;   
+}  
