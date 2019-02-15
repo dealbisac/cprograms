6f985dea93d681af318ac611ec68ea8788b7ee0f
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-15 07:08:44 +0545

    Questions Updated with Numbers

--- Challange-1/questions.cpp
+++ Challange-1/questions.cpp
@@ -1,18 +1,18 @@
 // Do the following program in your own way
 
-// C Program to Print a Sentence
-// C Program to Print a Integer Entered by a User
-// C Program to Add Two Integers Entered by User
-// C Program to Multiply two Floating Point Numbers
-// C Program to Find ASCII Value of Character Entered by User
-// C Program to Find Quotient and Remainder of Two Integers Entered by User
-// C Program to Find Size of int, float, double and char of Your System
-// C Program to Demonstrate the Working of Keyword long
-// C Program to Swap Two numbers Entered by User
-// C Program to show the example of ternary operator
-// C Program to show the examples of operators
-// C Program to show the example of escape sequence
-// C Program to show the example of Constant Value.
+// 1. C Program to Print a Sentence
+// 2. C Program to Print a Integer Entered by a User
+// 3. C Program to Add Two Integers Entered by User
+// 4. C Program to Multiply two Floating Point Numbers
+// 5. C Program to Find ASCII Value of Character Entered by User
+// 6. C Program to Find Quotient and Remainder of Two Integers Entered by User
+// 7. C Program to Find Size of int, float, double and char of Your System
+// 8. C Program to Demonstrate the Working of Keyword long
+// 9. C Program to Swap Two numbers Entered by User
+// 10. C Program to show the example of ternary operator
+// 11. C Program to show the examples of operators
+// 12. C Program to show the example of escape sequence
+// 13. C Program to show the example of Constant Value.
 
 
  
