8a5706bd8e56bb3eafe72c31f3fc2fca0c0eb7db
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-15 07:19:25 +0545

    solutions 2 added

---
+++ Challange-1/solution-2.cpp
@@ -0,0 +1,13 @@
+// 2. C Program to Print a Integer Entered by a User
+
+#include<stdio.h>
+int main(){
+	int num;
+	
+	printf("Enter the number: ");
+	scanf("%d", &num);
+	
+	printf("\n You entered %d", num);
+	
+	return 0;
+}
