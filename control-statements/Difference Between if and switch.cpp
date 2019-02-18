9cc6219702603550316e602919c9cec3f00c9646
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-18 06:51:40 +0545

    Difference Between If and Switch

---
+++ control-statements/Difference Between if and switch.cpp
@@ -0,0 +1,26 @@
+//Key Differences Between if-else and switch
+/*
+
+1. Expression inside if statement decide whether to execute the statements inside if block or under else block. 
+On the other hand, expression inside switch statement decide which case to execute.
+
+2. You can have multiple if statement for multiple choice of statements. 
+In switch you only have one expression for the multiple choices.
+
+3. If-esle statement checks for equality as well as for logical expression . 
+On the other hand, switch checks only for equality.
+
+4. The if statement evaluates integer, character, pointer or floating-point type or boolean type. 
+On the other hand, switch statement evaluates only character or a integer datatype.
+
+5. Sequence of execution is like either statement under if block will execute or statements under  else block statement will execute. 
+On the other hand the expression in switch statement  decide which case to execute and if you do not apply a break statement after each case 
+it will execute till the end of switch statement.
+
+6. If expression inside if turn outs to be false, statement inside else block will be executed. 
+If expression inside switch statement turn out to be false then default statements is executed.
+
+It is difficult to edit if-else statements as it is tedious to trace where the correction is required. 
+On the other hand it is easy to edit switch statements as they are easy to trace.
+
+*/
