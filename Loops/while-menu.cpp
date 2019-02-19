9876916c8f32e0302fb70971b81961cd57b0247c
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-19 07:44:49 +0545

    While menu added

---
+++ Loops/while-menu.cpp
@@ -0,0 +1,26 @@
+// C Program to show the menu until user exits
+
+#include<stdio.h>
+int main()
+{
+	
+	int choice;
+	
+	printf("Enter your choice: ");
+	scanf("%d", &choice);
+	
+	while(1)
+	{
+		printf("\n 1. Balance Enquiry");
+		printf("\n 2. Cash Withdrawal");
+		printf("\n 3. Fast Cash");
+		printf("\n 4. Mini Statement");
+		printf("\n 5. Exit");
+		printf("\nEnter your choice: ");
+		scanf("%d", &choice);
+	}
+	
+	printf("Thanks for being here");
+	
+	return 0;
+}
