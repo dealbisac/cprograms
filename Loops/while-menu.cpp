3f74bb8beddcd49ef977a31d1f7f0f684e733aca
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-19 07:55:01 +0545

    While menu updated

--- Loops/while-menu.cpp
+++ Loops/while-menu.cpp
@@ -1,10 +1,13 @@
 // C Program to show the menu until user exits
 
 #include<stdio.h>
+#include<stdlib.h> //used for exit(0)
 int main()
 {
 	
 	int choice;
+	
+	printf("\n Welcome to DBC Bank ");
 	
 	while(1)
 	{
@@ -15,7 +18,36 @@ int main()
 		printf("\n 5. Exit");
 		printf("\nEnter your choice: ");
 		scanf("%d", &choice);
-	}
+		
+		switch(choice)
+		{
+			case 1:
+				printf("\n You choose Balance Enquiry Option");
+				break;
+			
+			case 2:
+				printf("\n You choose Cash Withdrawal Option");
+				break;
+				
+			case 3:
+				printf("\n You choose Fast Cash Option");
+				break;
+			
+			case 4:
+				printf("\n You choose Mini Statement Option");
+				break;
+			
+			case 5:
+				printf("\n You are about to terminate");
+				exit(0);
+			
+			default:
+				printf("Please enter between (1-5)");	
+			 
+		} // end of switch statements
+		
+		
+	} // end of while loop
 	
 	printf("Thanks for being here");
 	
