633a6dcc1b6d1d9fe411602fcbd7ec3d565a9a59
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-21 07:08:03 +0545

    For Factorial Added

---
+++ Loops/for-factorial.cpp
@@ -0,0 +1,21 @@
+// C Program to find the factorial of the number using while loop
+
+# include<stdio.h>
+int main()
+{
+	int num, number, result=1;
+	
+	printf("\n Enter your number: ");
+	scanf("%d", &num);
+	
+	number = num;
+	
+	for(num=num; num>=1; num--)
+	{
+		result = result*num;
+	}
+	
+	printf("The factorial of %d is %d.", number, result);
+	
+	return 0;
+}
