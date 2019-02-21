488b79f5733515f97b8e526f34b3afd98811e83a
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-21 06:41:20 +0545

    For Loop Example Added

---
+++ Loops/for-example.cpp
@@ -0,0 +1,14 @@
+// C Program to show the example of For Loop
+
+#include<stdio.h>  
+int main()
+{  
+int i=0; 
+       
+for(i=1; i<=10; i=i*2)
+{      
+printf("%d \n",i);      
+}  
+   
+return 0;  
+}    
