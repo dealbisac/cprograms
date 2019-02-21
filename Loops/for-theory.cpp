5ec99ff97bfd981815d48a94bbaa91c8b674ae07
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-02-21 06:36:29 +0545

    For Loop Theory Added

---
+++ Loops/for-theory.cpp
@@ -0,0 +1,13 @@
+/*
+for loop in C
+The for loop in C language is used to iterate the statements or a part of the program several times.
+ It is frequently used to traverse the data structures like the array and linked list.
+
+Syntax of for loop in C
+The syntax of for loop in c language is given below:
+
+for(Expression 1; Expression 2; Expression 3){  
+//code to be executed  
+}  
+
+*/
