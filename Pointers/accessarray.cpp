3673f26074317511f5c2a964fcfdaa433fcf55fb
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-03-13 07:44:10 +0545

    Array Access

---
+++ Pointers/accessarray.cpp
@@ -0,0 +1,17 @@
+//Access Array Elements Using Pointers
+#include <stdio.h>
+
+int main()
+{
+   int data[5], i;
+   printf("Enter elements: ");
+
+   for(i = 0; i < 5; ++i)
+     scanf("%d", data + i);
+
+   printf("You entered: \n");
+   for(i = 0; i < 5; ++i)
+      printf("%d\n", *(data + i));
+
+   return 0;
+}
