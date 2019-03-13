ed3131681e22af3b840231288212cd3ba991b5b1
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2019-03-13 08:00:35 +0545

    Simple Array Example

---
+++ Pointers/simplearray.cpp
@@ -0,0 +1,16 @@
+//Simple Array Pointers Example
+#include <stdio.h>
+int main()
+{
+   int x[4];
+   int i;
+
+   for(i = 0; i < 4; ++i)
+   {
+      printf("&x[%d] = %u\n", i, &x[i]);
+   }
+
+   printf("Address of array x: %u", x);
+
+   return 0;
+}
